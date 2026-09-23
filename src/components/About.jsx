bf72d896343016ea2b09ec1af3f6d80e40c4883d
tamilarasi <tamilarasi@gmail.com>
2026-09-23 11:23:16 +0530

    initial

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,7 +20,7 @@ export default function About() {
       icon1: "./assets/edu-icon.png",
       icon2: "./assets/edu-icon-dark.png",
       description:
-        "B.Tech in Information Technology, Hindustan College of Engineering and Technology, Coimbatore",
+        "B.SC,Batchlor of science,Vivekanandha arts and science college,salem",
     },
     {
       name: "Projects",
